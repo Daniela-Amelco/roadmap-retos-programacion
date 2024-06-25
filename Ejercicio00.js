fe645be4d7a0b45efd168f31d78c12a84463113d
Daniela-Amelco <61768332+Daniela-Amelco@users.noreply.github.com>
2024-06-25 01:13:28 -0600

    Add files via upload

---
+++ Ejercicio00.js
@@ -0,0 +1,34 @@
+/* 1- Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado*/
+	/* https://www.javascript.com */
+
+/* 2- Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...)*/
+
+	//Comentario en una sola linea
+
+	/*Comentario en 
+	diferentes
+	lineas*/
+
+// 3- Declaración de variable
+	let nombre = "Dani";
+
+// 4- Declaración de una constante (Su valor no cambia)
+	const pi = 3.14;
+
+// 5- Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...). 
+	//variable tipo string	
+		let texto = "Hola";
+
+	//Variable tipo number
+		let edad = 9;
+
+	//Variable tipo boolean (indica valor verdadero o falso)
+		let bool1 = true;
+		let bool2 = false;
+
+	//Variable tipo null
+		let name = null;
+
+// 6- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+		let lenguaje = "JavaScript";
+		console.log("Hola mundo"+ lenguaje);
